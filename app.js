8ef9ccad30f0ad13727c6a7b81bfb3cc82054b0a
lucas baiba <lucasbaiba@iMac-de-lucas.local>
2021-11-01 13:40:00 +0100

    Front OK

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const port = process.env.PORT || 3000;
 // default option
 app.use(fileUpload());
 
+// Static folder
+app.use(express.static('public'));
+app.use(express.static('upload'));
+
 // Templating engine
 app.engine('hbs', exphbs({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
@@ -15,6 +19,31 @@ app.set('view engine', 'hbs');
 
 app.get('', (req, res) => {
   res.render('index');
+});
+
+app.post('', (req, res) => {
+  let sampleFile;
+  let uploadPath;
+
+  if(!req.files || Object.keys(req.files).length === 0){
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  // name of the input is sampleFile
+  sampleFile = req.files.sampleFile;
+  uploadPath = __dirname + '/upload/' + sampleFile.name;
+
+
+  console.log(sampleFile);
+
+  // Use mv() to place file on the server
+  sampleFile.mv(uploadPath, function(err) {
+    if(err) return res.status(500).send(err);
+
+    res.send('File uploaded!');
+
+  });
+
 });
 
 
